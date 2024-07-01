a0f58125f61d905870388dd01b2c289de2bdb9da
dappnodedev <diego@dappnode.io>
2024-07-01 13:51:26 +0200

    Use staker scripts

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+SCRIPTS_DIR=$(dirname "$0")
+CONSENSUS_TOOLS_SCRIPT="${SCRIPTS_DIR}/consensus-tools.sh"
+
+if [ ! -f "$CONSENSUS_TOOLS_SCRIPT" ]; then
+    echo "consensus-tools.sh not found"
+    exit 1
+fi
+
+# shellcheck disable=SC1090 # Shellcheck can't find the file dynamically
+. "${CONSENSUS_TOOLS_SCRIPT}"
+
 handle_network() {
     case "$NETWORK" in
     "gnosis") set_network_specific_config 19012 ;;
@@ -12,118 +23,31 @@ handle_network() {
     esac
 }
 
-set_network_specific_config() {
-    P2P_PORT=$1
+run_beacon() {
+    echo "[INFO - entrypoint] Running beacon node"
 
-    echo "[INFO - entrypoint] Initializing $NETWORK specific config for client"
-
-    set_execution_dnp
-
-    set_engine_url
-}
-
-set_execution_dnp() {
-    uppercase_network=$(echo "$NETWORK" | tr '[:lower:]' '[:upper:]')
-    execution_dnp_var="_DAPPNODE_GLOBAL_EXECUTION_CLIENT_${uppercase_network}"
-    eval "EXECUTION_DNP=\$$execution_dnp_var"
-}
-
-set_engine_url() {
-    case "$EXECUTION_DNP" in
-    *".public."*)
-        # nethermind.public.dappnode.eth -> nethermind.public
-        execution_subdomain=$(echo "$EXECUTION_DNP" | cut -d'.' -f1-2)
-        ;;
-    *)
-        # geth.dnp.dappnode.eth -> geth
-        execution_subdomain=$(echo "$EXECUTION_DNP" | cut -d'.' -f1)
-        ;;
-    esac
-
-    HTTP_ENGINE="http://${execution_subdomain}.dappnode:8551"
-}
-
-set_checkpointsync_url() {
-    if [ -n "$CHECKPOINT_SYNC_URL" ]; then
-        echo "[INFO - entrypoint] Checkpoint sync URL is set to $CHECKPOINT_SYNC_URL"
-        EXTRA_OPTS="${EXTRA_OPTS} --checkpointSyncUrl=${CHECKPOINT_SYNC_URL}"
-    else
-        echo "[WARN - entrypoint] Checkpoint sync URL is not set"
-    fi
-}
-
-# MEV-Boost: https://chainsafe.github.io/lodestar/usage/mev-integration/
-set_mev_boost_flag() {
-
-    network_uppercase=$(echo "${NETWORK}" | tr '[:lower:]' '[:upper:]')
-    mevboost_enabled_var="_DAPPNODE_GLOBAL_MEVBOOST_${network_uppercase}"
-
-    # Using eval to check and assign the variable, ensuring it's not unbound
-    eval "mevboost_enabled=\${${mevboost_enabled_var}:-false}"
-
-    # shellcheck disable=SC2154
-    if [ "${mevboost_enabled}" = "true" ]; then
-        echo "[INFO - entrypoint] MEV Boost is enabled"
-        set_mev_boost_url
-
-        if is_mev_boost_available; then
-            EXTRA_OPTS="--builder --builder.url=${MEVBOOST_URL} ${EXTRA_OPTS}"
-        fi
-    else
-        echo "[INFO - entrypoint] MEV Boost is disabled"
-    fi
-}
-
-set_mev_boost_url() {
-    # If network is mainnet and MEV-Boost is enabled, set the MEV-Boost URL
-    if [ "${NETWORK}" = "mainnet" ]; then
-        MEVBOOST_URL="http://mev-boost.dappnode:18550"
-    else
-        MEVBOOST_URL="http://mev-boost-${NETWORK}.dappnode:18550"
-    fi
-
-    echo "[INFO - entrypoint] MEV Boost URL is set to $MEVBOOST_URL"
-}
-
-is_mev_boost_available() {
-    if [ -z "${MEVBOOST_URL}" ]; then
-        echo "[ERROR - entrypoint] MEV Boost URL is not set"
-        return 1
-    fi
-
-    if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
-        echo "[INFO - entrypoint] MEV Boost is available"
-        return 0
-    else
-        echo "[ERROR - entrypoint] MEV Boost is enabled but the package at ${MEVBOOST_URL} is not reachable. Disabling MEV Boost..."
-        curl -X POST -G 'http://my.dappnode/notification-send' \
-            --data-urlencode 'type=danger' \
-            --data-urlencode title="${MEVBOOST_URL} can not be reached" \
-            --data-urlencode 'body=Make sure the MEV Boost DNP for this network is available and running'
-        return 1
-    fi
+    # shellcheck disable=SC2086 # (EXTRA_OPTS may be empty)
+    exec node --max-old-space-size="${MEMORY_LIMIT}" /usr/app/packages/cli/bin/lodestar \
+        beacon \
+        --network=mainnet \
+        --suggestedFeeRecipient="${FEE_RECIPIENT_ADDRESS}" \
+        --jwt-secret="${JWT_SECRET_FILE}" \
+        --execution.urls="${HTTP_ENGINE}" \
+        --dataDir="${DATA_DIR}" \
+        --rest \
+        --rest.address 0.0.0.0 \
+        --rest.port "${BEACON_API_PORT}" \
+        --port "${P2P_PORT}" \
+        --metrics \
+        --metrics.port 8008 \
+        --metrics.address 0.0.0.0 \
+        --logFile "${DATA_DIR}/beacon.log" \
+        --logLevel="${DEBUG_LEVEL}" \
+        --logFileLevel="${DEBUG_LEVEL}" \
+        --logFileDailyRotate 5 ${EXTRA_OPTS:-}
 }
 
 handle_network
-set_checkpointsync_url
-set_mev_boost_flag
-
-exec node --max-old-space-size="${MEMORY_LIMIT}" /usr/app/node_modules/.bin/lodestar \
-    beacon \
-    --network=mainnet \
-    --suggestedFeeRecipient="${FEE_RECIPIENT_ADDRESS}" \
-    --jwt-secret="${JWT_SECRET_FILE}" \
-    --execution.urls="${HTTP_ENGINE}" \
-    --dataDir="${DATA_DIR}" \
-    --rest \
-    --rest.address 0.0.0.0 \
-    --rest.port "${BEACON_API_PORT}" \
-    --port "${P2P_PORT}" \
-    --metrics \
-    --metrics.port 8008 \
-    --metrics.address 0.0.0.0 \
-    --logFile "${DATA_DIR}/beacon.log" \
-    --logLevel="${DEBUG_LEVEL}" \
-    --logFileLevel="${DEBUG_LEVEL}" \
-    --logFileDailyRotate 5 \
-    "${EXTRA_OPTS}"
+set_checkpointsync_url "--checkpointSyncUrl"
+set_mevboost "--builder --builder.url" # MEV-Boost: https://chainsafe.github.io/lodestar/usage/mev-integration/
+run_beacon
--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -1,65 +1,42 @@
 #!/bin/sh
 
-format_graffiti() {
-    # Save current locale settings
-    oLang="$LANG" oLcAll="$LC_ALL"
+SCRIPTS_DIR=$(dirname "$0")
+CONSENSUS_TOOLS_SCRIPT="${SCRIPTS_DIR}/consensus-tools.sh"
 
-    # Set locale to C for consistent behavior in string operations
-    LANG=C LC_ALL=C
+if [ ! -f "$CONSENSUS_TOOLS_SCRIPT" ]; then
+    echo "consensus-tools.sh not found"
+    exit 1
+fi
 
-    if [ -z "$GRAFFITI" ]; then
-        VALID_GRAFFITI=""
-    else
-        # Truncate GRAFFITI to 32 characters if it is set
-        VALID_GRAFFITI=$(echo "$GRAFFITI" | cut -c 1-32)
-    fi
+# shellcheck disable=SC1090 # Shellcheck can't find the file dynamically
+. "${CONSENSUS_TOOLS_SCRIPT}"
 
-    echo "[INFO] Using graffiti: $VALID_GRAFFITI"
+run_validator() {
+    echo "[INFO - entrypoint] Running validator node"
 
-    # Restore locale settings
-    LANG="$oLang" LC_ALL="$oLcAll"
-}
-
-# MEV-Boost: https://chainsafe.github.io/lodestar/usage/mev-integration/
-set_mev_boost_flag() {
-
-    network_uppercase=$(echo "${NETWORK}" | tr '[:lower:]' '[:upper:]')
-    mevboost_enabled_var="_DAPPNODE_GLOBAL_MEVBOOST_${network_uppercase}"
-
-    # Using eval to check and assign the variable, ensuring it's not unbound
-    eval "mevboost_enabled=\${${mevboost_enabled_var}:-false}"
-
-    # shellcheck disable=SC2154
-    if [ "${mevboost_enabled}" = "true" ]; then
-        echo "[INFO] MEV Boost is enabled"
-        EXTRA_OPTS="--builder ${EXTRA_OPTS}"
-    else
-        echo "[INFO] MEV Boost is disabled"
-    fi
+    # shellcheck disable=SC2086
+    exec node /usr/app/node_modules/.bin/lodestar \
+        validator \
+        --network="${NETWORK}" \
+        --suggestedFeeRecipient="${FEE_RECIPIENT_ADDRESS}" \
+        --graffiti="${GRAFFITI}" \
+        --dataDir="${DATA_DIR}" \
+        --keymanager true \
+        --keymanager.authEnabled true \
+        --keymanager.port 3500 \
+        --keymanager.address 0.0.0.0 \
+        --metrics \
+        --metrics.port 5064 \
+        --metrics.address 0.0.0.0 \
+        --externalSigner.url="${HTTP_WEB3SIGNER}" \
+        --doppelgangerProtection="${DOPPELGANGER_PROTECTION}" \
+        --beaconNodes="${BEACON_NODE_ADDR}" \
+        --logLevel="${DEBUG_LEVEL}" \
+        --logFileLevel=debug \
+        --logFileDailyRotate 5 \
+        --logFile /var/lib/data/validator.log ${EXTRA_OPTS}
 }
 
 format_graffiti
-
-set_mev_boost_flag
-
-exec node /usr/app/node_modules/.bin/lodestar \
-    validator \
-    --network="${NETWORK}" \
-    --suggestedFeeRecipient="${FEE_RECIPIENT_ADDRESS}" \
-    --graffiti="${VALID_GRAFFITI}" \
-    --dataDir="${DATA_DIR}" \
-    --keymanager true \
-    --keymanager.authEnabled true \
-    --keymanager.port 3500 \
-    --keymanager.address 0.0.0.0 \
-    --metrics \
-    --metrics.port 5064 \
-    --metrics.address 0.0.0.0 \
-    --externalSigner.url="${HTTP_WEB3SIGNER}" \
-    --doppelgangerProtection="${DOPPELGANGER_PROTECTION}" \
-    --beaconNodes="${BEACON_NODE_ADDR}" \
-    --logLevel="${DEBUG_LEVEL}" \
-    --logFileLevel=debug \
-    --logFileDailyRotate 5 \
-    --logFile /var/lib/data/validator.log \
-    "${EXTRA_OPTS}"
+set_mevboost "--builder" "true" # MEV-Boost: https://chainsafe.github.io/lodestar/usage/mev-integration/
+run_validator
