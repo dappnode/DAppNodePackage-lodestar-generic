d16748c27c34bd74b06ade4ab0ae383b13f99edd
dappnodedev <diego@dappnode.io>
2024-07-19 15:32:58 +0200

    Use stakers scripts getter functions

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -2,36 +2,34 @@
 
 SUPPORTED_NETWORKS="gnosis holesky mainnet"
 CHECKPOINT_SYNC_FLAG="--checkpointSyncUrl"
-MEVBOOST_FLAGS="--builder --builder.url"
+MEVBOOST_FLAG_KEYS="--builder --builder.url"
 
 # shellcheck disable=SC1091 # Path is relative to the Dockerfile
 . /etc/profile
 
-run_beacon() {
-    echo "[INFO - entrypoint] Running beacon node"
+ENGINE_URL=$(get_engine_api_url "${NETWORK}" "${SUPPORTED_NETWORKS}")
+VALID_FEE_RECIPIENT=$(get_valid_fee_recipient "${FEE_RECIPIENT}")
+CHECKPOINT_SYNC_FLAG=$(get_checkpoint_sync_flag "${CHECKPOINT_SYNC_FLAG}" "${CHECKPOINT_SYNC_URL}")
+MEVBOOST_FLAG=$(get_mevboost_flag "${NETWORK}" "${MEVBOOST_FLAG_KEYS}")
 
-    # shellcheck disable=SC2086 # (EXTRA_OPTS may be empty)
-    exec ${CLIENT_BIN} \
-        beacon \
-        --network="${NETWORK}" \
-        --suggestedFeeRecipient="${FEE_RECIPIENT}" \
-        --jwt-secret="${JWT_FILE_PATH}" \
-        --execution.urls="${ENGINE_API_URL}" \
-        --dataDir="${DATA_DIR}" \
-        --rest \
-        --rest.address 0.0.0.0 \
-        --rest.port "${BEACON_API_PORT}" \
-        --port "${P2P_PORT}" \
-        --metrics \
-        --metrics.port 8008 \
-        --metrics.address 0.0.0.0 \
-        --logFile "${DATA_DIR}/beacon.log" \
-        --logLevel="${LOG_LEVEL}" \
-        --logFileLevel=debug \
-        --logFileDailyRotate 5 ${EXTRA_OPTS}
-}
+echo "[INFO - entrypoint] Running beacon node"
 
-set_beacon_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}"
-set_checkpoint_sync_url "${CHECKPOINT_SYNC_FLAG}" "${CHECKPOINT_SYNC_URL}"
-set_mevboost_flag "${MEVBOOST_FLAGS}" # MEV-Boost: https://chainsafe.github.io/lodestar/usage/mev-integration/
-run_beacon
+# shellcheck disable=SC2086 # (EXTRA_OPTS may be empty)
+exec ${CLIENT_BIN} \
+    beacon \
+    --network="${NETWORK}" \
+    --suggestedFeeRecipient="${VALID_FEE_RECIPIENT}" \
+    --jwt-secret="${JWT_FILE_PATH}" \
+    --execution.urls="${ENGINE_URL}" \
+    --dataDir="${DATA_DIR}" \
+    --rest \
+    --rest.address 0.0.0.0 \
+    --rest.port "${BEACON_API_PORT}" \
+    --port "${P2P_PORT}" \
+    --metrics \
+    --metrics.port 8008 \
+    --metrics.address 0.0.0.0 \
+    --logFile "${DATA_DIR}/beacon.log" \
+    --logLevel="${LOG_LEVEL}" \
+    --logFileLevel=debug \
+    --logFileDailyRotate 5 ${CHECKPOINT_SYNC_FLAG} ${MEVBOOST_FLAG} ${EXTRA_OPTS}
