d5f65d118f54a173912759dac18ddde13b24daf3
dappnodedev <diego@dappnode.io>
2024-08-07 16:12:57 +0200

    Use JWT from staker tools

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-SUPPORTED_NETWORKS="gnosis holesky mainnet"
 CHECKPOINT_SYNC_FLAG="--checkpointSyncUrl"
 MEVBOOST_FLAG_KEYS="--builder --builder.url"
 
 # shellcheck disable=SC1091 # Path is relative to the Dockerfile
 . /etc/profile
 
-ENGINE_URL=$(get_engine_api_url "${NETWORK}" "${SUPPORTED_NETWORKS}")
+ENGINE_URL="http://execution.${NETWORK}.staker.dappnode:8551"
 VALID_FEE_RECIPIENT=$(get_valid_fee_recipient "${FEE_RECIPIENT}")
 CHECKPOINT_SYNC_FLAG=$(get_checkpoint_sync_flag "${CHECKPOINT_SYNC_FLAG}" "${CHECKPOINT_SYNC_URL}")
 MEVBOOST_FLAG=$(get_mevboost_flag "${NETWORK}" "${MEVBOOST_FLAG_KEYS}")
+
+JWT_SECRET=$(get_jwt_secret_by_network "${NETWORK}")
+echo "${JWT_SECRET}" >"${JWT_FILE_PATH}"
 
 echo "[INFO - entrypoint] Running beacon node"
 
