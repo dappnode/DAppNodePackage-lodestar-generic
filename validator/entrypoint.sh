9098495a6e0177ba83cf8bf8c9d82d94fe011348
dappnodedev <diego@dappnode.io>
2024-09-24 12:11:26 +0200

    Fix validator MEV Boost flag

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -12,7 +12,7 @@ VALID_GRAFFITI=$(get_valid_graffiti "${GRAFFITI}")
 VALID_FEE_RECIPIENT=$(get_valid_fee_recipient "${FEE_RECIPIENT}")
 SIGNER_API_URL=$(get_signer_api_url "${NETWORK}" "${SUPPORTED_NETWORKS}")
 BEACON_API_URL=$(get_beacon_api_url "${NETWORK}" "${SUPPORTED_NETWORKS}" "${CLIENT}")
-MEVBOOST_FLAG=$(get_mevboost_flag "${MEVBOOST_FLAG_KEY}" "${SKIP_MEVBOOST_URL}")
+MEVBOOST_FLAG=$(get_mevboost_flag "${NETWORK}" "${MEVBOOST_FLAG_KEY}" "${SKIP_MEVBOOST_URL}")
 
 FLAGS="validator \
     --network=${NETWORK} \
